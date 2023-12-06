79b862fc6de8b29006e08604be3fcf3fd312e9c8
youcanlost <youcanlosts@nave.comm>
2023-12-06 19:58:27 +0900

    4th

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,15 +1,24 @@
 import {useState} from "react";
+import Calendar from 'react-calendar';
+import moment from 'moment';
+import 'react-calendar/dist/Calendar.css';
+
 
 
 const Todo = () => {
     const [todo, setTodo] = useState([]);
     const [content, setContent] = useState("")
+    const [Calendarinstance, onChange] = useState("")
+
+    const caldate = new Date()
 
     const onRegister = () => {
         setTodo([...todo, {
             title: content,
+            cdate: Calendarinstance,
             isComplete: false
         }]);
+        console.log("item.cdate",Calendarinstance);
         setContent("")
     }
 
@@ -27,6 +36,11 @@ const Todo = () => {
 
     return (
         <div style={{height: "100vh", display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: "lightblue"}}>
+            
+            <div>
+            <Calendar onChange={onChange} value={Calendarinstance} />
+            </div>
+
             <div style={{backgroundColor: "white", padding: 50, display: 'flex', borderRadius: 20, flexDirection: "column"}}>
             <div style={{fontSize: 40}}>To Do List</div>
             <div>
@@ -40,26 +54,43 @@ const Todo = () => {
             </div>
             {
                 todo.map((item, index) => {
-                    return (
-                        <div style={{
-                            textDecoration: item.isComplete ? "line-through" : "none",
-                            display: 'flex',
-                            flexDirection: 'row'
-                        }}>
-                            <div>
-                                {item.title}
-                            </div>
-                            {
-                                item.isComplete ||
+
+                    
+                    console.log(item.title, moment(Calendarinstance).format("YYYYMMDD") ,"/",moment(item.cdate).format("YYYYMMDD"));
+
+                    if(moment(item.cdate).format("YYYYMMDD") === moment(Calendarinstance).format("YYYYMMDD")) 
+                    {
+                        return (
+                            <div style={{
+                                textDecoration: item.isComplete ? "line-through" : "none",
+                                display: 'flex',
+                                flexDirection: 'row'
+                            }}>
+                                <div style={{display: 'flex', flexDirection: 'row'}}>
                                     <div>
-                                        <button onClick={() => onComplete(index)}>완료</button>
+                                        {item.title}
                                     </div>
-                            }
-                            <div>
-                                <button onClick={() => onDelete(index)}>삭제</button>
+                                    {
+                                        item.isComplete ||
+                                            <div>
+                                                <button onClick={() => onComplete(index)}>완료</button>
+                                            </div>
+                                    }
+                                    <div>
+                                        <button onClick={() => onDelete(index)}>삭제</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    )
+
+                        )
+                    }
+                    else
+                    {
+                        return
+                    }
+
+
+
                 })
             }
             </div>
